8d0962cd9a2808e4a73413c58e9f26fd14badb1c
MiroshnykovDaniil <danielq911q@gmail.com>
2019-08-18 10:05:12 +0300

    Miroshnikov daniel (#14)
    * Adding my classes
    
    * 1. Создал корректный бренч
    2. Убрал delete (оставил deleteById)
    3. Перенес @Transactional из Controller в Repository
    4. Убрал тесты из main
    5. убрал ProjectService за ненадобностью
    6. Создал ProjectDTO, использовале его в Controller
    7. Для связки Project и ProjectDTO создал ProjectMapper
    8. Конвертировал properties в yml

---
+++ students-registry/projects-service/src/main/java/boost/brain/course/Main.java
@@ -0,0 +1,14 @@
+package boost.brain.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class Main
+{
+    public static void main( String[] args ) throws IOException {
+        SpringApplication.run(Main.class, args);
+    }
+}

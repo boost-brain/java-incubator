c243896782e79618c2774b57ad2f02c0423071d3
ggi-dev <ggi.mail.for.web@gmail.com>
2019-08-19 13:25:36 +0600

    fix prefix in Constants.java

--- students-registry/tasks-service/src/main/java/boost/brain/course/tasks/Constants.java
+++ students-registry/tasks-service/src/main/java/boost/brain/course/tasks/Constants.java
@@ -3,7 +3,7 @@ package boost.brain.course.tasks;
 public interface Constants {
     String API_PREFIX = "/api";
 
-    String EXAMPLE_CONTROLLER_PREFIX = API_PREFIX + "/tasks";
+    String TASKS_CONTROLLER_PREFIX = API_PREFIX + "/tasks";
     String CREATE_PREFIX = "/create";
     String READ_PREFIX = "/read";
     String UPDATE_PREFIX = "/update";

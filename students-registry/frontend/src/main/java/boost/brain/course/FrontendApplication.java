d9140eb9042ba3b32e05188f3c222af3732bd4df
ggi-dev <ggi.mail.for.web@gmail.com>
2019-08-22 14:16:47 +0600

    add Dockerfile and ZuulProxy

--- students-registry/frontend/src/main/java/boost/brain/course/FrontendApplication.java
+++ students-registry/frontend/src/main/java/boost/brain/course/FrontendApplication.java
@@ -2,7 +2,9 @@ package boost.brain.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class FrontendApplication {
 

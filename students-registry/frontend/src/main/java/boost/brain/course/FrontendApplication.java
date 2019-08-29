95672085de3278810fd8964d0b80fd3816595930
sergeyag <NOTES_8@HNBA100215.moscow.alfaintra.net>
2019-08-29 10:48:27 +0300

    пересоздал ветку и внес dockerfile

--- students-registry/frontend/src/main/java/boost/brain/course/FrontendApplication.java
+++ students-registry/frontend/src/main/java/boost/brain/course/FrontendApplication.java
@@ -2,9 +2,7 @@ package boost.brain.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
 @SpringBootApplication
 public class FrontendApplication {
 

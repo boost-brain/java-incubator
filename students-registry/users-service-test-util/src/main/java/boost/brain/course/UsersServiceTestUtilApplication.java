f8d14c8de844b47ddfbd4f8756d80f75e170633b
Roman Stoyko <roman.stoyko@gmail.com>
2019-09-06 12:28:31 +0300

    issue_41 (#50)
    * issue_41
    
    * issue_41
    add application.yaml
    add Thread.yield(); in execute methods in MainController
    remove var 'done' in execute methods in MainController

---
+++ students-registry/users-service-test-util/src/main/java/boost/brain/course/UsersServiceTestUtilApplication.java
@@ -0,0 +1,57 @@
+package boost.brain.course;
+
+
+import boost.brain.course.controller.MainController;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Application work's with command line.
+ * Can execute commands:
+ * -url - full url with http://
+ * -o - operations what can be done
+ *     count - return count of rows in table
+ *     create - send generated JSON to add a new record
+ *     delete - get 20 records from random page and delete them
+ *     page - get 20 records from random page
+ *     read - get 20 records from random page and read records
+ *     update - get 20 records from random page and update record with new generated data
+ *
+ *     default command = count
+ *
+ * -c - count of operations what will be execute
+ *      default = 1000
+ *
+ * -ext - exit application
+ *
+ * example
+ * -url=http://localhost:8082 -o=create -c=100
+ * -url=http://localhost:8082 -o=read -c=10
+ * -url=http://localhost:8082 -o=delete -c=15
+ * -url=http://localhost:8082 -o=update -c=4
+ * -url=http://localhost:8082 -o=page -c=100
+ *
+*/
+@Log
+@SpringBootApplication
+public class UsersServiceTestUtilApplication implements CommandLineRunner {
+
+    @Autowired
+    private ApplicationContext context;
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersServiceTestUtilApplication.class, args);
+    }
+
+    @Override
+    public void run(String[] args) throws Exception {
+        MainController mainController = new MainController();
+        mainController.run();
+        SpringApplication.exit(context);
+    }
+}
+

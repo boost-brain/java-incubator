6683c643bab8028f36ac04ccc4f2c7e03486897c
kardinaldon <kardinalmail@gmail.com>
2019-10-03 20:29:05 +0300

    fix bugs5

--- students-registry/jsf-frontend/src/main/java/boost/brain/course/frontend/administrator/createUser/service/CreateUsersService.java
+++ students-registry/jsf-frontend/src/main/java/boost/brain/course/frontend/administrator/createUser/service/CreateUsersService.java
@@ -4,6 +4,7 @@ import boost.brain.course.common.auth.Credentials;
 import boost.brain.course.common.auth.UserDto;
 import boost.brain.course.frontend.register.model.User;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +17,19 @@ import java.io.Serializable;
 
 @Named
 @RequestScope
-@Service
 public class CreateUsersService implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private String urlUsers = "http://users-service:8080/api/users/create";
+
+    @Value("${users-service-url}")
+    private String urlUsers;
+
+
+//    @Value("${auth-login-url}")
+//    private String urlAuth;
+
+    //private String urlUsers = "http://users-service:8080/api/users/create";
     private String urlAuth = "http://auth-service:8080/api/credentials/create";
 
     private RestTemplate restTemplate;
@@ -30,8 +38,6 @@ public class CreateUsersService implements Serializable {
     private Credentials credentials;
     String resultMessage;
 
-    @RequestMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
-            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public void addNewUser(String name, String email, String password, String gitHabId, int hours) {
 
         user = new User(name, email, password, gitHabId, hours);
@@ -43,7 +49,7 @@ public class CreateUsersService implements Serializable {
 
         try {
             restTemplate = new RestTemplate();
-            restTemplate.postForObject(urlUsers, userDto, UserDto.class);
+            restTemplate.postForObject(urlUsers+"/create", userDto, UserDto.class);
             addNewCredentials(email, password);
         } catch (HttpStatusCodeException ex) {
             System.out.println(ex.getStatusCode());
@@ -51,8 +57,6 @@ public class CreateUsersService implements Serializable {
         }
     }
 
-    @RequestMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
-            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public void addNewCredentials(String email, String password) {
         credentials = new Credentials(email, password);
         try {

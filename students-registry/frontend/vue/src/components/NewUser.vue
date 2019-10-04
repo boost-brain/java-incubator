cb73239f6bbe0e5e5a86703d672dfe920fead10d
Sergey <sergeyag@live.ru>
2019-10-04 18:39:10 +0300

    NewUser validations were added

--- students-registry/frontend/vue/src/components/NewUser.vue
+++ students-registry/frontend/vue/src/components/NewUser.vue
@@ -5,34 +5,54 @@
                 <h1 class="text--secondary mb-3">Регистрация</h1>
                 <v-form ref="form" validation class="mb-3">
                     <v-text-field
-                            name="Email:"
-                            label="email"
+                            autocomplete="off"
+                            name="email"
+                            label="Email:"
                             type="text"
+                            :class="{'is-invalid': $v.email.$error}"
+                            @blur="$v.email.$touch()"
+                            :error-messages="$v.email.$dirty && !$v.email.required ? ['Обязательно к заполнению'] : !$v.email.email ? ['Введите корректный email'] : []"
                             v-model="email"
                     ></v-text-field>
                     <v-text-field
-                            name="Id на github.com:"
-                            label="gitHabId"
+                            name="gitHubId"
+                            label="Id на github.com:"
                             type="text"
-                            v-model="gitHabId"
+                            :class="{'is-invalid': $v.gitHubId.$error}"
+                            @blur="$v.gitHubId.$touch()"
+                            :error-messages="$v.gitHubId.$dirty && !$v.gitHubId.required ? ['Обязательно к заполнению'] : []"
+                            v-model="gitHubId"
                     ></v-text-field>
                     <v-text-field
                             label="Ваше имя:"
                             name="name"
                             type="text"
+                            :class="{'is-invalid': $v.name.$error}"
+                            @blur="$v.name.$touch()"
+                            :error-messages="$v.name.$dirty && !$v.name.required ? ['Обязательно к заполнению'] : []"
                             v-model="name"
                     ></v-text-field>
                     <v-text-field
+                            name="hours"
                             label="Сколько часов в день в среднем Вы готовы уделять проекту?"
                             v-model="hours"
+                            :class="{'is-invalid': $v.hours.$error}"
+                            @blur="$v.hours.$touch()"
+                            :error-messages="$v.hours.$dirty && !$v.hours.required ? ['Обязательно к заполнению'] : !$v.hours.numeric ? ['Введите числовое значение'] : !$v.hours.minValue ? ['Число часов должно быть больше 1'] : []"
                             type="text">
                     </v-text-field>
                     <v-text-field
                             label="Задайте пароль:"
                             v-model="password"
-                            type="password"
+                            :append-icon="show ? 'visibility' : 'visibility_off'"
+                            @click:append="show = !show"
+                            :type="show ? 'text' : 'password'"
+                            :class="{'is-invalid': $v.password.$error}"
+                            @blur="$v.password.$touch()"
+                            :error-messages="!$v.password.minLength ? ['Минимальная длина пароля 6'] : []"
                             required>
                     </v-text-field>
+<!--                    <pre>{{ $v.password }}</pre>-->
                 </v-form>
                 <v-layout row>
                     <v-flex xs12>
@@ -53,16 +73,18 @@
 <script>
 
     import { mapActions } from 'vuex'
+    import { required, email, numeric, minLength, minValue } from 'vuelidate/lib/validators'
 
     export default {
         name: "NewUser",
         data () {
             return {
                 email: '',
-                gitHabId: '',
+                gitHubId: '',
                 name: '',
                 password: '',
-                hours: 0,
+                show: false,
+                hours: 1,
                 createDate: 0,
                 updateDate: 0
             }
@@ -76,7 +98,7 @@
                         email: this.email,
                         password: this.password,
                         createDate: this.createDate,
-                        gitHabId: this.gitHabId,
+                        gitHubId: this.gitHubId,
                         hours: this.hours,
                         name: this.name,
                         updateDate: this.updateDate
@@ -84,6 +106,26 @@
                     this.registerAction(user, "user")
                     this.$router.push('/login')
                 }
+            }
+        },
+        validations: {
+            email: {
+                required,
+                email
+            },
+            gitHubId: {
+                required
+            },
+            name: {
+                required
+            },
+            hours: {
+                required,
+                numeric,
+                minValue: minValue(1)
+            },
+            password: {
+                minLength: minLength(6)
             }
         }
     }
--- students-registry/frontend/vue/src/main.js
+++ students-registry/frontend/vue/src/main.js
@@ -4,10 +4,12 @@ import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import Vuelidate from 'vuelidate'
 import VueResource from 'vue-resource'
 
 
 Vue.use(VueResource)
+Vue.use(Vuelidate)
 
 new Vue({
     router,

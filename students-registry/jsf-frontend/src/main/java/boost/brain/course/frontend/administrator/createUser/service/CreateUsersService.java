3f47bca69e79323b0a3ea35e6eb0fabea58609a5
kardinaldon <kardinalmail@gmail.com>
2019-10-06 15:03:18 +0300

    fix bugs5

--- students-registry/jsf-frontend/src/main/java/boost/brain/course/frontend/administrator/createUser/service/CreateUsersService.java
+++ students-registry/jsf-frontend/src/main/java/boost/brain/course/frontend/administrator/createUser/service/CreateUsersService.java
@@ -1,15 +1,16 @@
 package boost.brain.course.frontend.administrator.createUser.service;
 
 import boost.brain.course.common.auth.Credentials;
-
 import boost.brain.course.common.users.UserDto;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.annotation.RequestScope;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import java.io.Serializable;
 
 @Named
 @RequestScope
@@ -29,7 +30,6 @@ public class CreateUsersService {
 //    @Value("${auth-login-url}")
 //    private String urlAuth;
 
-    //private String urlUsers = "http://users-service:8080/api/users/create";
     private String urlAuth = "http://auth-service:8080/api/credentials/create";
 
     private RestTemplate restTemplate;
@@ -38,20 +38,34 @@ public class CreateUsersService {
     String resultMessage;
 
     public void addNewUser(String name, String email, String password, String gitHabId, int hours) {
+        if (name != null && email != null && password != null && gitHabId != null) {
+            userDto.setEmail(email);
+            userDto.setGitHabId(gitHabId);
+            userDto.setName(name);
+            userDto.setHours(hours);
 
-        userDto.setEmail(email);
-        userDto.setGitHabId(gitHabId);
-        userDto.setName(name);
-        userDto.setHours(hours);
+            try {
+                restTemplate = new RestTemplate();
+                UserDto userDtoResp = restTemplate.postForObject(urlUsers + "/create", this.userDto, UserDto.class);
+                if (userDtoResp != null) {
+                    addNewCredentials(email, password);
+                } else {
+                    resultMessage = "User " + userDto.getName() + " not created";
+                    FacesMessage warningMessage = new FacesMessage(FacesMessage.SEVERITY_WARN,
+                            "Warning summary", "Warning detail");
+                    FacesContext.getCurrentInstance().addMessage("", warningMessage);
+                }
 
+            } catch (HttpStatusCodeException ex) {
+                System.out.println(ex.getStatusCode());
+                ex.printStackTrace();
+            }
 
-        try {
-            restTemplate = new RestTemplate();
-            restTemplate.postForObject(urlUsers+"/create", userDto, UserDto.class);
-            addNewCredentials(email, password);
-        } catch (HttpStatusCodeException ex) {
-            System.out.println(ex.getStatusCode());
-            ex.printStackTrace();
+        } else {
+            FacesMessage infoMessage = new FacesMessage(FacesMessage.SEVERITY_INFO,
+                    "Info summary", "Info detail");
+            FacesContext.getCurrentInstance().addMessage("", infoMessage);
+            resultMessage = "Fill in all the fields";
         }
     }
 

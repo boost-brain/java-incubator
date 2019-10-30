541816e6d68871eeedd71ee5a1c7669927913470
cardinal <kardinalmail@gmail.com>
2019-10-30 16:07:22 +0300

    fix bugs9

--- students-registry/jsf-frontend/src/main/java/boost/brain/course/frontend/administrator/createUser/CreateUsers.java
+++ students-registry/jsf-frontend/src/main/java/boost/brain/course/frontend/administrator/createUser/CreateUsers.java
@@ -5,7 +5,11 @@ import boost.brain.course.common.users.UserDto;
 
 import lombok.Data;
 import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.annotation.RequestScope;
 
@@ -16,18 +20,19 @@ import javax.inject.Named;
 @Named
 @RequestScope
 @Data
-@Log
+@Slf4j
 public class CreateUsers {
 
     private String name;
     private String email;
     private String password;
     private String gitHubId;
-    private int hours;
+    private Integer hours;
 
-    private RestTemplate restTemplate;
+    private RestTemplate restTemplate = new RestTemplate();
     private UserDto userDto = new UserDto();
-    private Credentials credentials = new Credentials();;
+    private Credentials credentials = new Credentials();
+
 
     FacesContext context = FacesContext.getCurrentInstance();
     FacesMessage message;
@@ -38,40 +43,28 @@ public class CreateUsers {
     @Value("${auth-credentials-url}")
     private String urlAuth;
 
-    public void addNewUser(String name, String email, String password, String gitHubId, int hours) {
+    public void addNewUser(String name, String email, String password, String gitHubId, Integer hours) {
         if (!name.isEmpty() & !email.isEmpty() & !password.isEmpty() & !gitHubId.isEmpty()) {
+            if(hours==null) {
+                hours=1;
+            }
             userDto.setEmail(email);
             userDto.setGitHubId(gitHubId);
             userDto.setName(name);
             userDto.setHours(hours);
 
-            try {
-                restTemplate = new RestTemplate();
-                UserDto userDtoResp = restTemplate.postForObject(urlUsers + "/create", this.userDto, UserDto.class);
-                if (userDtoResp != null) {
-                    addNewCredentials(email, password);
-                } else {
-                    message = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-                            "Внимание !!!",
-                            "Корректно заполните все поля");
-                    context.addMessage(null, message);
-                    context.getPartialViewContext().getRenderIds().add("globalMessage");
-                }
-            } catch (Exception ex) {
-                message = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-                        "Внимание !!!",
-                        "Корректно заполните все поля");
-                context.addMessage(null, message);
-                context.getPartialViewContext().getRenderIds().add("globalMessage");
-                System.out.println(ex.getCause());
+            UserDto userDtoResp = restTemplate.postForObject(urlUsers + "/create", this.userDto, UserDto.class);
+            if (userDtoResp != null) {
+                addNewCredentials(email, password);
+                log.info("Create user in table user_entity was success");
+            } else {
+                errorMessage(false);
+                log.error("Create user in table user_entity was failed");
             }
 
         } else {
-            message = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-                    "Внимание !!!",
-                    "Корректно заполните все поля");
-            context.addMessage(null, message);
-            context.getPartialViewContext().getRenderIds().add("globalMessage");
+            errorMessage(false);
+            log.error("one of the fields in JSF GUI is empty");
         }
     }
 
@@ -79,20 +72,26 @@ public class CreateUsers {
         credentials.setLogin(email);
         credentials.setPassword(password);
         try {
-            restTemplate = new RestTemplate();
             restTemplate.postForObject(urlAuth + "/create", credentials, Boolean.class);
-            message = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+            errorMessage(true);
+            log.info("Create credentials in table credentials_entity was success");
+        } catch (Exception ex) {
+            errorMessage(false);
+            log.error("Create credentials in table credentials_entity was failed" + ex.getCause());
+        }
+    }
+
+    private void errorMessage(boolean numberOfError) {
+        if (numberOfError) {
+            message = new FacesMessage(FacesMessage.SEVERITY_INFO,
                     "Пользователь создан !!!",
                     "Данные внесены корректно");
-            context.addMessage(null, message);
-            context.getPartialViewContext().getRenderIds().add("globalMessage");
-        } catch (Exception ex) {
+        } else {
             message = new FacesMessage(FacesMessage.SEVERITY_ERROR,
                     "Внимание !!!",
                     "Корректно заполните все поля");
-            context.addMessage(null, message);
-            context.getPartialViewContext().getRenderIds().add("globalMessage");
-            System.out.println(ex.getCause());
         }
+        context.addMessage(null, message);
+        context.getPartialViewContext().getRenderIds().add("globalMessage");
     }
 }

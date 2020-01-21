1e8a471181238b85afb1ba76bca30eeebda77728
Oleg Pavlyukov <cpabox777@gmail.com>
2020-01-21 23:03:20 +0300

    Add tests for the UserController

---
+++ students-registry/users-service/src/test/java/boost/brain/course/usersTest/controller/UsersControllerTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Oleg Pavlyukov
+ * on 21.01.2020
+ * cpabox777@gmail.com
+ */
+class UsersControllerTest {
+
+}
---
+++ students-registry/users-service/src/test/java/boost/brain/course/usersTest/utils/UserDtoProviderUtil.java
@@ -0,0 +1,9 @@
+package boost.brain.course.usersTest.utils;
+
+/**
+ * @author Oleg Pavlyukov
+ * on 21.01.2020
+ * cpabox777@gmail.com
+ */
+public class UserDtoProviderUtil {
+}

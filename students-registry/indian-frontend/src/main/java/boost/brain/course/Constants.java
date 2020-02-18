d13284a20f824bf3001ff28d9195d08dfaec18fb
bjcreslin <bjcreslin@gmail.com>
2020-02-18 22:18:36 +0700

    localhost

--- students-registry/indian-frontend/src/main/java/boost/brain/course/Constants.java
+++ students-registry/indian-frontend/src/main/java/boost/brain/course/Constants.java
@@ -5,7 +5,7 @@ public interface Constants {
     String USERCONTROLLER_PREFIX = "users/";
     String SERVER_ADDRESS = "http://185.255.135.104/";
     String AUTH_SERVER = "http://185.255.135.104:8081/";
-    String USER_SERVER = "http://185.255.135.104:8082/";
+    String USER_SERVER = "http://localhost:8082/";
     String PROJECT_SERVER = "http://185.255.135.104:8083/";
     String TASK_SERVER = "http://185.255.135.104:8084/";
     String REG_SERVER = "http://185.255.135.104:8086/";

f0d79f4953461ae1779b18a5e36a4cf4f4e6ef71
ggi-dev <53460439+ggi-dev@users.noreply.github.com>
2020-08-28 08:57:21 +0600

    Внедрение UserRole (#168)
    Co-authored-by: ggi-dev <ggi.mail.for.web@gmail.com>

--- students-registry/common-types/src/main/java/boost/brain/course/common/auth/Credentials.java
+++ students-registry/common-types/src/main/java/boost/brain/course/common/auth/Credentials.java
@@ -1,9 +1,13 @@
 package boost.brain.course.common.auth;
 
+import boost.brain.course.common.users.UserRole;
 import lombok.Data;
+
+import java.util.Set;
 
 @Data
 public class Credentials {
     private String login;
     private String password;
+    private Set<UserRole> roles;
 }

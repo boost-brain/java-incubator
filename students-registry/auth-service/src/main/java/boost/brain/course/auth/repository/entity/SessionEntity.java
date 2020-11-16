aa1cb45a7c472884bc75299229ac1f1c61cd06e1
ggi-dev <ggi.mail.for.web@gmail.com>
2020-11-16 16:14:15 +0600

    вынес генерацию sessionId из репозитория в сущность Session с использванием аннотаций над полем id

--- students-registry/auth-service/src/main/java/boost/brain/course/auth/repository/entity/SessionEntity.java
+++ students-registry/auth-service/src/main/java/boost/brain/course/auth/repository/entity/SessionEntity.java
@@ -2,16 +2,18 @@ package boost.brain.course.auth.repository.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
 public class SessionEntity {
     @Id
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid2")
+    @Column(insertable = false, updatable = false, nullable = false)
     private String id;
     private long startTime;
     private long validTime;
